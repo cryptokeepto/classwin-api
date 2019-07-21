fadc312e44264606b7ef352ba10d7adb24129a0e
cryptokeepto <cryptokeepto@gmail.com>
2019-07-21 13:44:14 +0700

    add lodash

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 const PORT = 3001;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/api", (req, res) => {
-	return res.end("ok"); 
+app.post("/api/on-live-auth", (req, res) => {
+	const streamInfo = req.body
+	const streamSecretKey = _.get(streamInfo, "name")
+	console.log(streamSecretKey);
+	console.log(_.get(streamInfo, "call"))
+	return res.status(200).json({ lived: true }); 
+})
+
+app.post("/api/on-live-done", (req, res) => {
+	const streamInfo = req.body
+	console.log(_.get(streamInfo, "call"));
+	return res.status(200).json({ lived: false });
 })
 
 app.listen(PORT, () => {
